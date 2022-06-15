e4504bc2744eeb90d1ce00a83235bb2f424730b7
Camille Arsac <camille.arsac26@gmail.com>
2022-06-15 14:31:21 +0200

    chore(CI): implementation of CI

--- src/infrastructure/views/react-ui/.eslintrc.js
+++ src/infrastructure/views/react-ui/.eslintrc.js
@@ -1,17 +1,20 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-  },
-  extends: "plugin:react/recommended",
-  parser: "@typescript-eslint/parser",
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
+    "env": {
+        "browser": true,
+        "es2021": true
     },
-    ecmaVersion: "latest",
-  },
-  plugins: ["react", "@typescript-eslint"],
-  rules: {},
-  root: true,
-};
+    "extends": "plugin:react/recommended",
+    "parser": "@typescript-eslint/parser",
+    "parserOptions": {
+        "ecmaFeatures": {
+            "jsx": true
+        },
+        "ecmaVersion": "latest"
+    },
+    "plugins": [
+        "react",
+        "@typescript-eslint"
+    ],
+    "rules": {
+    }
+}
