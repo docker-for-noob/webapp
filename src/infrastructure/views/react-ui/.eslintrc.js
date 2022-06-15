bee4fb80b3dc386e9435b1269755e4bfe63cbc22
Camille-Arsac <Camille-Arsac@users.noreply.github.com>
2022-06-15 20:43:07 +0000

    chore(prettier): commit auto to formatted code

--- src/infrastructure/views/react-ui/.eslintrc.js
+++ src/infrastructure/views/react-ui/.eslintrc.js
@@ -1,20 +1,16 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: "plugin:react/recommended",
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
     },
-    "extends": "plugin:react/recommended",
-    "parser": "@typescript-eslint/parser",
-    "parserOptions": {
-        "ecmaFeatures": {
-            "jsx": true
-        },
-        "ecmaVersion": "latest"
-    },
-    "plugins": [
-        "react",
-        "@typescript-eslint"
-    ],
-    "rules": {
-    }
-}
+    ecmaVersion: "latest",
+  },
+  plugins: ["react", "@typescript-eslint"],
+  rules: {},
+};
