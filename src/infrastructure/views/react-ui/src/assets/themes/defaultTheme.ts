8db9e9fe9d6ea8b3c254a073e6b1f02550032c7b
Artpou <Artpou@users.noreply.github.com>
2022-09-05 09:53:56 +0000

    chore(prettier): commit auto to formatted code

--- src/infrastructure/views/react-ui/src/assets/themes/defaultTheme.ts
+++ src/infrastructure/views/react-ui/src/assets/themes/defaultTheme.ts
@@ -1,12 +1,12 @@
-import { createTheme, Theme } from '@mui/material';
+import { createTheme, Theme } from "@mui/material";
 
 export const defaultTheme: Theme = createTheme({
   palette: {
     primary: {
-      main: '#6563ff',
+      main: "#6563ff",
     },
     secondary: {
-      main: '#D8D8FF',
+      main: "#D8D8FF",
     },
   },
   typography: {
--- src/infrastructure/views/react-ui/src/mock/HelperMock.ts
+++ src/infrastructure/views/react-ui/src/mock/HelperMock.ts
@@ -1,29 +1,29 @@
 import { HelperData } from "src/components/Helper";
 
 export const mockHelpers: Array<HelperData> = [
-    {
-        title: 'Services',
-        content: 'Entrer un nom de service',
-        link: 'https://docs.docker.com/engine/userguide/dockervolumes/',
-    },
-    {
-        title: "Choix de l'image",
-        content: 'Rechercher une image et choisir les tags',
-        link: 'https://docs.docker.com/engine/userguide/dockervolumes/',
-    },
-    {
-        title: 'Ports',
-        content: 'Définir les ports',
-        link: 'https://docs.docker.com/engine/userguide/dockervolumes/',
-    },
-    {
-        title: 'Volumes',
-        content: 'Ajouter des volumes',
-        link: 'https://docs.docker.com/engine/userguide/dockervolumes/',
-    },
-    {
-        title: "Variables d'environnement",
-        content: "Définir les variables d'environnement",
-        link: 'https://docs.docker.com/engine/userguide/dockervolumes/'
-    }
+  {
+    title: "Services",
+    content: "Entrer un nom de service",
+    link: "https://docs.docker.com/engine/userguide/dockervolumes/",
+  },
+  {
+    title: "Choix de l'image",
+    content: "Rechercher une image et choisir les tags",
+    link: "https://docs.docker.com/engine/userguide/dockervolumes/",
+  },
+  {
+    title: "Ports",
+    content: "Définir les ports",
+    link: "https://docs.docker.com/engine/userguide/dockervolumes/",
+  },
+  {
+    title: "Volumes",
+    content: "Ajouter des volumes",
+    link: "https://docs.docker.com/engine/userguide/dockervolumes/",
+  },
+  {
+    title: "Variables d'environnement",
+    content: "Définir les variables d'environnement",
+    link: "https://docs.docker.com/engine/userguide/dockervolumes/",
+  },
 ];
