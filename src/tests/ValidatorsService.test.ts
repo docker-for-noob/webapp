2fc1d01f6840e160c661f70890ec97de02a696da
Hijoxx <mr.leroyflorian@gmail.com>
2022-09-26 18:44:34 +0200

    fix: coverage config

--- src/tests/ValidatorsService.test.ts
+++ src/tests/ValidatorsService.test.ts
@@ -20,6 +20,10 @@ import {
   versionUIValidator,
   volumesUIValidator,
 } from "../infrastructure/validators/InputValidator";
+import {ValidatorService} from "../core/domain/dockerCompose/service/validator/ValidatorService"
+
+
+import {mustBeString, required} from "../infrastructure/validators/ValidatorDependance";
 
 test("no ports has been sent and return an error", () => {
   const initial = undefined;
@@ -249,4 +253,12 @@ test("a valid volume path has been sent and return undefined", () => {
 test("an unvalid volume path has been sent and return an error message", () => {
   const initial = "C:/hel**o";
   expect(envVariablePathUIValidator(initial)).toStrictEqual(mustBePathError);
+});
+
+
+
+
+test("a service name with Special Character has been sent and return an error", () => {
+  const initial = "test";
+  expect(ValidatorService.portMustBeUnique(initial)).toStrictEqual(undefined);
 });
