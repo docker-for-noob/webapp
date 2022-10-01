dcc894341b5a4abdecef112168d288393ae94d90
Hijoxx <mr.leroyflorian@gmail.com>
2022-10-01 21:38:05 +0200

    fix : api call example

--- src/infrastructure/redux/api/endpoint.ts
+++ src/infrastructure/redux/api/endpoint.ts
@@ -1,1 +1,15 @@
-export const PLACEHOLDER_ARTICLES_URL = "posts";
+import { DockerHubRequest } from "./requestParams";
+
+const DockerImageArea = "dockerImage";
+const ImageReferenceArea = "reference";
+
+export const populateImagesEndpoint = `${DockerImageArea}/images`;
+export const populateVersionEndpoint = (image: string) =>
+    `${DockerImageArea}/versions/${image}`;
+export const getAllTagsFromImageVersion = ({
+                                               image,
+                                               version,
+                                           }: DockerHubRequest) => `${DockerImageArea}/tags/${image}/${version}`;
+
+export const GetImageReference = (image: string) =>
+    `${ImageReferenceArea}/${image}`;
---
+++ src/infrastructure/redux/api/requestParams.ts
@@ -0,0 +1,8 @@
+export type DockerHubRequest = {
+    image: string;
+    version: string;
+};
+
+export type imageParams = {
+    image: string;
+};
