700d21e6fcc346825b0e13e7d17f91e3ef8515e8
Cédric PA <t24nk1ll3r@gmail.com>
2022-10-12 15:38:46 +0200

    chore(mock): add lorem ipsum and proper links to helpers

--- src/infrastructure/views/react-ui/src/mock/HelperMock.ts
+++ src/infrastructure/views/react-ui/src/mock/HelperMock.ts
@@ -3,27 +3,27 @@ import { HelperData } from "src/components/Helper";
 export const mockHelpers: Array<HelperData> = [
   {
     title: "Services",
-    content: "Entrer un nom de service",
-    link: "https://docs.docker.com/engine/userguide/dockervolumes/",
+    content: "Incididunt et aliqua dolore ex quis minim consequat nisi officia esse minim nulla aute enim. Minim tempor mollit laborum aliqua proident ex amet esse consequat eu. Aliqua magna eu veniam occaecat quis ea. Tempor aliqua officia eiusmod sunt occaecat culpa cupidatat cillum nostrud sint id cillum est labore.",
+    link: "https://docs.docker.com/compose/compose-file/#services-top-level-element",
   },
   {
     title: "Choix de l'image",
-    content: "Rechercher une image et choisir les tags",
-    link: "https://docs.docker.com/engine/userguide/dockervolumes/",
+    content: "Commodo id laboris exercitation amet magna laborum sint duis ipsum. Ut occaecat est aute anim et est aliquip sint. Aute eiusmod incididunt cupidatat in laborum deserunt velit non. Cupidatat magna aute dolor id excepteur consectetur esse. Consequat aliqua commodo dolor eu aliquip elit amet consequat tempor.",
+    link: "https://docs.docker.com/compose/compose-file/#image",
   },
   {
     title: "Ports",
-    content: "Définir les ports",
-    link: "https://docs.docker.com/engine/userguide/dockervolumes/",
+    content: "Consequat proident nisi aute deserunt sit dolore amet amet magna anim. Et culpa aliquip exercitation fugiat consequat pariatur fugiat elit et. Elit reprehenderit consectetur anim Lorem esse mollit est reprehenderit ullamco ad.",
+    link: "https://docs.docker.com/compose/compose-file/#ports",
   },
   {
     title: "Volumes",
-    content: "Ajouter des volumes",
-    link: "https://docs.docker.com/engine/userguide/dockervolumes/",
+    content: "Adipisicing velit voluptate minim laboris id reprehenderit anim eiusmod eu Lorem. Dolore aliqua occaecat quis cillum et sit ad anim et duis minim reprehenderit sit. Anim enim nisi cillum officia ad laboris culpa sint nostrud anim et.",
+    link: "https://docs.docker.com/compose/compose-file/#volumes",
   },
   {
     title: "Variables d'environnement",
-    content: "Définir les variables d'environnement",
-    link: "https://docs.docker.com/engine/userguide/dockervolumes/",
+    content: "Cupidatat in ea reprehenderit laboris tempor. Sunt ex nisi ad officia sunt ullamco nostrud esse culpa id ut excepteur. Irure aute consectetur occaecat officia aute veniam anim adipisicing. Sunt consectetur ut irure sit magna labore ea do in do ad tempor. Duis enim excepteur nisi minim occaecat laborum. Cillum veniam incididunt Lorem nostrud nulla amet elit. Occaecat veniam elit enim qui et dolore mollit nulla sint dolor eu.",
+    link: "https://docs.docker.com/compose/compose-file/#environment",
   },
 ];
