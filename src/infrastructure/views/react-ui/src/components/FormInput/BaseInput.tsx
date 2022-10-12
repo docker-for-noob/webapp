d16ac0f23de1f70060a3f153434f0751045303bc
Cédric PA <t24nk1ll3r@gmail.com>
2022-10-12 16:06:24 +0200

    feat(BaseInput): add tooltip to text field

--- src/infrastructure/views/react-ui/src/components/FormInput/BaseInput.tsx
+++ src/infrastructure/views/react-ui/src/components/FormInput/BaseInput.tsx
@@ -3,6 +3,12 @@ import {
     FormControl,
     TextField,
     Typography,
+    Tooltip,
+    styled,
+    TooltipProps,
+    tooltipClasses,
+    Input,
+    FilledInput,
     useTheme
 } from "@mui/material";
 import {
@@ -21,27 +27,36 @@ interface InputTextFormProps {
     disabled?: boolean;
     error?: Maybe<string>;
 }
-
+const CustomWidthTooltip = styled(({ className, ...props }: TooltipProps) => (
+    <Tooltip {...props} classes={{ popper: className }} />
+))({
+    [`& .${tooltipClasses.tooltip}`]: {
+        maxWidth: 500,
+        height: 30
+    },
+});
 export function InputTextForm(props: InputTextFormProps) {
     const theme = useTheme()
     const color = acquireValidationColor(props.error)
 
     return (
-        <FormControl sx={{margin: '1rem 0'}}>
-            <TextField
-                label={props.label}
-                value={props.value}
-                type={props.type || 'text'}
-                variant={props.variant || 'standard'}
-                onChange={props.onChange}
-                disabled={props.disabled}
-                error={handleError(props.error)}
-                color={color}
-                focused={handleError(props.error)}
-                helperText={<Typography sx={{color: theme.palette[color!].main}}>
-                    {acquireHelperText(props.error)}
-                </Typography>}
-            />
+        <FormControl sx={{ margin: '1rem 0' }}>
+            <CustomWidthTooltip title='Nisi cillum laboris officia duis ut dolore culpa.' followCursor={true}>
+                <TextField
+                    label={props.label}
+                    value={props.value}
+                    type={props.type || 'text'}
+                    variant={props.variant || 'standard'}
+                    onChange={props.onChange}
+                    disabled={props.disabled}
+                    error={handleError(props.error)}
+                    color={color}
+                    focused={handleError(props.error)}
+                    helperText={<Typography sx={{ color: theme.palette[color!].main }}>
+                        {acquireHelperText(props.error)}
+                    </Typography>}
+                />
+            </CustomWidthTooltip>
         </FormControl>
     );
 }
